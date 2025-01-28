5d237b97b4906fdde070b1ad2ae1df545638dff1
Ltabsyy <111294086+Ltabsyy@users.noreply.github.com>
2025-01-29 00:29:03 +0800

    Upgrade to version 0.4

--- Nonogram 0.4.c
+++ Nonogram 0.4.c
@@ -9,19 +9,19 @@
 
 int isMine[LimHeight][LimWidth];
 int isOpen[LimHeight][LimWidth];
-//int solution[LimHeight][LimWidth];
+int solution[LimHeight][LimWidth];
 int rowNumber[LimHeight][(LimWidth+1)/2];
 int columnNumber[(LimHeight+1)/2][LimWidth];
 int rowNumberColor[LimHeight][(LimWidth+1)/2];
 int columnNumberColor[(LimHeight+1)/2][LimWidth];
 
 // 标准线
-/*int* lineMine = 0;
+int* lineMine = 0;
 int* lineOpen = 0;
 int* lineSolution = 0;
 int* lineNumber = 0;
 int lengthOfLine;
-int countOfLineNumber;*/
+int countOfLineNumber;
 
 int heightOfBoard = 6;
 int widthOfBoard = 6;
@@ -30,6 +30,8 @@ int summonCheckMode = 0;
 
 int lengthOfRowNumber = 3;
 int lengthOfColumnNumber = 3;
+
+int debug = 0;
 
 void ColorStr(const char* content, int color)//输出彩色字符
 {
@@ -78,6 +80,8 @@ void SetConsoleMouseMode(int mode)//键鼠操作切换
 			| ENABLE_AUTO_POSITION);
 	}
 }
+
+int Solve();
 
 void PrintBoard(int mode)
 {
@@ -192,6 +196,122 @@ void SummonBoard(int seed)
 				i++;
 			}
 		}
+		// 初始化数字
+		for(r=0; r<heightOfBoard; r++)
+		{
+			for(c=0; c<lengthOfRowNumber; c++)
+			{
+				rowNumber[r][c] = 0;
+			}
+		}
+		for(r=0; r<lengthOfColumnNumber; r++)
+		{
+			for(c=0; c<widthOfBoard; c++)
+			{
+				columnNumber[r][c] = 0;
+			}
+		}
+		// 计算行数字
+		for(r=0; r<heightOfBoard; r++)
+		{
+			n = 0;
+			i = lengthOfRowNumber-1;
+			for(c=widthOfBoard-1; c>=0; c--)
+			{
+				if(isMine[r][c] == 0)
+				{
+					if(n != 0)
+					{
+						rowNumber[r][i] = n;
+						n = 0;
+						i--;
+					}
+				}
+				else
+				{
+					n++;
+				}
+			}
+			if(n != 0)
+			{
+				rowNumber[r][i] = n;
+				n = 0;
+				i--;
+			}
+		}
+		// 计算列数字
+		for(c=0; c<widthOfBoard; c++)
+		{
+			n = 0;
+			i = lengthOfColumnNumber-1;
+			for(r=heightOfBoard-1; r>=0; r--)
+			{
+				if(isMine[r][c] == 0)
+				{
+					if(n != 0)
+					{
+						columnNumber[i][c] = n;
+						n = 0;
+						i--;
+					}
+				}
+				else
+				{
+					n++;
+				}
+			}
+			if(n != 0)
+			{
+				columnNumber[i][c] = n;
+				n = 0;
+				i--;
+			}
+		}
+		// 初始化颜色
+		for(r=0; r<heightOfBoard; r++)
+		{
+			for(c=0; c<lengthOfRowNumber; c++)
+			{
+				rowNumberColor[r][c] = 0x07;
+				if(rowNumber[r][c] != 0)
+				{
+					if(rowNumber[r][c] == widthOfBoard)
+					{
+						rowNumberColor[r][c] = 0x01;
+					}
+					else if(rowNumber[r][c] > widthOfBoard/2)
+					{
+						rowNumberColor[r][c] = 0x02;
+					}
+					else if(rowNumber[r][c] > widthOfBoard/3)
+					{
+						rowNumberColor[r][c] = 0x04;
+					}
+				}
+			}
+		}
+		for(r=0; r<lengthOfColumnNumber; r++)
+		{
+			for(c=0; c<widthOfBoard; c++)
+			{
+				columnNumberColor[r][c] = 0x07;
+				if(columnNumber[r][c] != 0)
+				{
+					if(columnNumber[r][c] == heightOfBoard)
+					{
+						columnNumberColor[r][c] = 0x01;
+					}
+					else if(columnNumber[r][c] > heightOfBoard/2)
+					{
+						columnNumberColor[r][c] = 0x02;
+					}
+					else if(columnNumber[r][c] > heightOfBoard/3)
+					{
+						columnNumberColor[r][c] = 0x04;
+					}
+				}
+			}
+		}
 		if(summonCheckMode == 0) break;
 		else if(summonCheckMode == 1)
 		{
@@ -252,149 +372,37 @@ void SummonBoard(int seed)
 		else if(summonCheckMode == 2)
 		{
 			//校验存在空线则退出
+			n = 0;
 			for(r=0; r<heightOfBoard; r++)
 			{
-				n = 0;
-				for(c=0; c<widthOfBoard; c++)
+				if(rowNumber[r][lengthOfRowNumber-1] == 0)
 				{
-					if(isMine[r][c] == 1)
-					{
-						n = 1;
-						break;
-					}
+					n = 1;
+					break;
 				}
-				if(n == 0) break;
 			}
-			if(n == 0) break;
 			for(c=0; c<widthOfBoard; c++)
 			{
-				n = 0;
+				if(columnNumber[lengthOfColumnNumber-1][c] == 0)
+				{
+					n = 1;
+					break;
+				}
+			}
+			if(n == 1) break;
+		}
+		else if(summonCheckMode == 3)
+		{
+			if(Solve() == 1)//校验地图可解
+			{
 				for(r=0; r<heightOfBoard; r++)
 				{
-					if(isMine[r][c] == 1)
+					for(c=0; c<widthOfBoard; c++)
 					{
-						n = 1;
-						break;
+						isOpen[r][c] = 0;
 					}
 				}
-				if(n == 0) break;
-			}
-			if(n == 0) break;
-		}
-	}
-	// 初始化数字
-	for(r=0; r<heightOfBoard; r++)
-	{
-		for(c=0; c<lengthOfRowNumber; c++)
-		{
-			rowNumber[r][c] = 0;
-		}
-	}
-	for(r=0; r<lengthOfColumnNumber; r++)
-	{
-		for(c=0; c<widthOfBoard; c++)
-		{
-			columnNumber[r][c] = 0;
-		}
-	}
-	// 计算行数字
-	for(r=0; r<heightOfBoard; r++)
-	{
-		n = 0;
-		i = lengthOfRowNumber-1;
-		for(c=widthOfBoard-1; c>=0; c--)
-		{
-			if(isMine[r][c] == 0)
-			{
-				if(n != 0)
-				{
-					rowNumber[r][i] = n;
-					n = 0;
-					i--;
-				}
-			}
-			else
-			{
-				n++;
-			}
-		}
-		if(n != 0)
-		{
-			rowNumber[r][i] = n;
-			n = 0;
-			i--;
-		}
-	}
-	// 计算列数字
-	for(c=0; c<widthOfBoard; c++)
-	{
-		n = 0;
-		i = lengthOfColumnNumber-1;
-		for(r=heightOfBoard-1; r>=0; r--)
-		{
-			if(isMine[r][c] == 0)
-			{
-				if(n != 0)
-				{
-					columnNumber[i][c] = n;
-					n = 0;
-					i--;
-				}
-			}
-			else
-			{
-				n++;
-			}
-		}
-		if(n != 0)
-		{
-			columnNumber[i][c] = n;
-			n = 0;
-			i--;
-		}
-	}
-	// 初始化颜色
-	for(r=0; r<heightOfBoard; r++)
-	{
-		for(c=0; c<lengthOfRowNumber; c++)
-		{
-			rowNumberColor[r][c] = 0x07;
-			if(rowNumber[r][c] != 0)
-			{
-				if(rowNumber[r][c] == widthOfBoard)
-				{
-					rowNumberColor[r][c] = 0x01;
-				}
-				else if(rowNumber[r][c] > widthOfBoard/2)
-				{
-					rowNumberColor[r][c] = 0x02;
-				}
-				else if(rowNumber[r][c] > widthOfBoard/3)
-				{
-					rowNumberColor[r][c] = 0x04;
-				}
-			}
-		}
-	}
-	for(r=0; r<lengthOfColumnNumber; r++)
-	{
-		for(c=0; c<widthOfBoard; c++)
-		{
-			columnNumberColor[r][c] = 0x07;
-			if(columnNumber[r][c] != 0)
-			{
-				if(columnNumber[r][c] == heightOfBoard)
-				{
-					columnNumberColor[r][c] = 0x01;
-				}
-				else if(columnNumber[r][c] > heightOfBoard/2)
-				{
-					columnNumberColor[r][c] = 0x02;
-				}
-				else if(columnNumber[r][c] > heightOfBoard/3)
-				{
-					columnNumberColor[r][c] = 0x04;
-				}
+				break;
 			}
 		}
 	}
@@ -416,7 +424,7 @@ int CheckSign()
 	}
 	return 1;//胜利
 }
-/*
+
 void RecoverLine(int r, int c, int mode)//0生成line，1写出
 {
 	int i, i0;
@@ -425,25 +433,40 @@ void RecoverLine(int r, int c, int mode)//0生成line，1写出
 		if(mode == 0)
 		{
 			lengthOfLine = widthOfBoard;
-			if(lineMine != 0) lineMine =(int*) calloc(lengthOfLine, sizeof(int));
-			if(lineOpen != 0) lineOpen =(int*) calloc(lengthOfLine, sizeof(int));
-			if(lineSolution != 0) lineSolution =(int*) calloc(lengthOfLine, sizeof(int));
+			if(lineMine != 0) free(lineMine);
+			if(lineOpen != 0) free(lineOpen);
+			if(lineSolution != 0) free(lineSolution);
+			lineMine =(int*) calloc(lengthOfLine, sizeof(int));
+			lineOpen =(int*) calloc(lengthOfLine, sizeof(int));
+			lineSolution =(int*) calloc(lengthOfLine, sizeof(int));
 			for(i=0; i<lengthOfLine; i++)
 			{
 				lineMine[i] = isMine[r][i];
 				lineOpen[i] = isOpen[r][i];
-				lineSolution[i] = solution[r][i];
+				//lineSolution[i] = solution[r][i];
 			}
 			countOfLineNumber = (lengthOfLine+1)/2;
-			for(i=0; i<countOfLineNumber; i++)
+			for(i=0; i<(lengthOfLine+1)/2; i++)
 			{
 				if(rowNumber[r][i] == 0) countOfLineNumber--;
 				else break;
 			}
-			if(lineNumber != 0) lineNumber =(int*) calloc(countOfLineNumber, sizeof(int));
-			for(i0=i; i<countOfLineNumber; i++)
+			if(lineNumber != 0) free(lineNumber);
+			lineNumber =(int*) calloc(countOfLineNumber, sizeof(int));
+			for(i0=i; i<(lengthOfLine+1)/2; i++)
 			{
 				lineNumber[i-i0] = rowNumber[r][i];
+			}
+		}
+		else if(mode == 1)
+		{
+			for(i=0; i<lengthOfLine; i++)
+			{
+				if(lineSolution[i] != 0)
+				{
+					solution[r][i] = lineSolution[i];
+					isOpen[r][i] = lineSolution[i];
+				}
 			}
 		}
 	}
@@ -452,25 +475,40 @@ void RecoverLine(int r, int c, int mode)//0生成line，1写出
 		if(mode == 0)
 		{
 			lengthOfLine = heightOfBoard;
-			if(lineMine != 0) lineMine =(int*) calloc(lengthOfLine, sizeof(int));
-			if(lineOpen != 0) lineOpen =(int*) calloc(lengthOfLine, sizeof(int));
-			if(lineSolution != 0) lineSolution =(int*) calloc(lengthOfLine, sizeof(int));
+			if(lineMine != 0) free(lineMine);
+			if(lineOpen != 0) free(lineOpen);
+			if(lineSolution != 0) free(lineSolution);
+			lineMine =(int*) calloc(lengthOfLine, sizeof(int));
+			lineOpen =(int*) calloc(lengthOfLine, sizeof(int));
+			lineSolution =(int*) calloc(lengthOfLine, sizeof(int));
 			for(i=0; i<lengthOfLine; i++)
 			{
 				lineMine[i] = isMine[i][c];
 				lineOpen[i] = isOpen[i][c];
-				lineSolution[i] = solution[i][c];
+				//lineSolution[i] = solution[i][c];
 			}
 			countOfLineNumber = (lengthOfLine+1)/2;
-			for(i=0; i<countOfLineNumber; i++)
+			for(i=0; i<(lengthOfLine+1)/2; i++)
 			{
 				if(columnNumber[i][c] == 0) countOfLineNumber--;
 				else break;
 			}
-			if(lineNumber != 0) lineNumber =(int*) calloc(countOfLineNumber, sizeof(int));
-			for(i0=i; i<countOfLineNumber; i++)
+			if(lineNumber != 0) free(lineNumber);
+			lineNumber =(int*) calloc(countOfLineNumber, sizeof(int));
+			for(i0=i; i<(lengthOfLine+1)/2; i++)
 			{
 				lineNumber[i-i0] = columnNumber[i][c];
+			}
+		}
+		else if(mode == 1)
+		{
+			for(i=0; i<lengthOfLine; i++)
+			{
+				if(lineSolution[i] != 0)
+				{
+					solution[i][c] = lineSolution[i];
+					isOpen[i][c] = lineSolution[i];
+				}
 			}
 		}
 	}
@@ -489,12 +527,9 @@ struct LinesIterator LinesIteratorBegin()
 	return li;
 }
 
-struct LinesIterator LinesIteratorEnd()//超尾
+int IsLinesIteratorEnd(struct LinesIterator li)//判断超尾
 {
-	struct LinesIterator li;
-	li.r = -1;
-	li.c = -1;
-	return li;
+	return li.r == -1 && li.c == -1;
 }
 
 void LinesIteratorNext(struct LinesIterator* li)
@@ -517,7 +552,313 @@ void LinesIteratorNext(struct LinesIterator* li)
 		}
 	}
 }
-*/
+
+int PutNumber(int* mine, int pos, int number)//尝试放置数字，基于标准线信息
+{
+	int i;
+	//判断无法放置情况
+	if(pos + number > lengthOfLine) return 0;//超出边界
+	if(pos + number < lengthOfLine && lineOpen[pos+number] == 2) return 0;//尾部为标记
+	if(pos > 0 && lineOpen[pos-1] == 2) return 0;//头部为标记
+	for(i=pos; i<pos+number; i++)
+	{
+		if(lineOpen[i] == 1) return 0;//中部被翻开
+	}
+	//放置
+	for(i=pos; i<pos+number; i++)
+	{
+		mine[i] = 1;
+	}
+	//if(pos + number < lengthOfLine) mine[i] = 0;
+	return 1;
+}
+
+int* LineFirstSolutionMine()//生成标准线首解雷场
+{
+	int* mine =(int*) calloc(lengthOfLine, sizeof(int));
+	int i, pos = 0;
+	for(i=0; i<countOfLineNumber; )
+	{
+		if(PutNumber(mine, pos, lineNumber[i]) == 1)
+		{
+			pos += lineNumber[i]+1;
+			i++;
+		}
+		else
+		{
+			pos++;
+			//if(pos + lineNumber[i] > lengthOfLine)//无解情况，暂不考虑
+		}
+	}
+	return mine;
+}
+
+int* LineLastSolutionMine()//生成标准线末解雷场
+{
+	int* mine =(int*) calloc(lengthOfLine, sizeof(int));
+	int i, pos = lengthOfLine-1;
+	for(i=countOfLineNumber-1; i>=0; )
+	{
+		if(PutNumber(mine, pos, lineNumber[i]) == 1)
+		{
+			if(i > 0) pos -= lineNumber[i-1]+1;
+			i--;
+		}
+		else
+		{
+			pos--;
+			//if(pos < 0)//无解情况，暂不考虑
+		}
+	}
+	return mine;
+}
+
+int SolveLine(struct LinesIterator li)
+{
+	int i, ni;
+	//生成标准线
+	RecoverLine(li.r, li.c, 0);
+	if(debug)
+	{
+		printf("定位标准线(%d,%d)\n", li.r, li.c);
+		/*printf("lengthOfLine=%d\n", lengthOfLine);
+		printf("countOfLineNumber=%d\n", countOfLineNumber);
+		printf("lineMine：");
+		for(i=0; i<lengthOfLine; i++)
+		{
+			printf("%d ", lineMine[i]);
+		}
+		printf("\n");
+		printf("lineOpen：");
+		for(i=0; i<lengthOfLine; i++)
+		{
+			printf("%d ", lineOpen[i]);
+		}
+		printf("\n");
+		printf("lineSolution：");
+		for(i=0; i<lengthOfLine; i++)
+		{
+			printf("%d ", lineSolution[i]);
+		}
+		printf("\n");*/
+		printf("lineNumber：");
+		for(i=0; i<countOfLineNumber; i++)
+		{
+			printf("%d ", lineNumber[i]);
+		}
+		printf("\n");
+	}
+	//端向心分析
+	int nipos;
+	ni = 0;
+	for(i=0; i<lengthOfLine; i++)//头向尾分析
+	{
+		if(lineOpen[i] == 0) break;
+		else if(lineOpen[i] == 2)
+		{
+			for(nipos = i; i < nipos+lineNumber[ni]; i++)//顺延标记
+			{
+				if(lineOpen[i] != 2) lineSolution[i] = 2;
+			}
+			if(nipos+lineNumber[ni] < lengthOfLine && lineOpen[nipos+lineNumber[ni]] != 1)//尾部翻开
+			{
+				lineSolution[nipos+lineNumber[ni]] = 1;
+			}
+			ni++;
+			//i定位到超尾之后
+			if(ni == countOfLineNumber)
+			{
+				for(; i<lengthOfLine; i++)//持续翻开
+				{
+					if(lineOpen[i] != 1) lineSolution[i] = 1;
+				}
+			}
+		}
+	}
+	ni = countOfLineNumber-1;
+	for(i=lengthOfLine-1; i>=0; i--)//尾向头分析
+	{
+		if(lineOpen[i] == 0) break;
+		else if(lineOpen[i] == 2)
+		{
+			for(nipos = i; i > nipos-lineNumber[ni]; i--)//顺延标记
+			{
+				if(lineOpen[i] != 2) lineSolution[i] = 2;
+			}
+			if(nipos-lineNumber[ni] >= 0 && lineOpen[nipos-lineNumber[ni]] != 1)//头部翻开
+			{
+				lineSolution[nipos-lineNumber[ni]] = 1;
+			}
+			ni--;
+			if(ni < 0)
+			{
+				for(; i>=0; i--)//持续翻开
+				{
+					if(lineOpen[i] != 1) lineSolution[i] = 1;
+				}
+			}
+		}
+	}
+	//首末解交汇分析
+	int* firstMine = LineFirstSolutionMine();//生成首解雷场
+	int* lastMine = LineLastSolutionMine();//生成末解雷场
+	int* hasfirstSolution =(int*) calloc(countOfLineNumber, sizeof(int));//数字首解存在栈，用于回溯定位
+	int matchFirstLast = 1;
+	for(i=0; i<lengthOfLine; i++)
+	{
+		if(firstMine[i] != lastMine[i]) matchFirstLast = 0;
+	}
+	if(matchFirstLast == 1)//首解末解完全相同，可判断端收束顶满线
+	{
+		for(i=0; i<lengthOfLine; i++)
+		{
+			if(firstMine[i] == 1 && lineOpen[i] != 2) lineSolution[i] = 2;
+			if(firstMine[i] == 0 && lineOpen[i] != 1) lineSolution[i] = 1;
+		}
+	}
+	else
+	{
+		//简单数字分析，可判断大数半满偏移
+		int n1pos = 0, n2pos = 0;
+		for(ni=0; ni<countOfLineNumber; ni++)
+		{
+			while(n1pos+lineNumber[ni] < lengthOfLine && firstMine[n1pos] == 0) n1pos++;
+			while(n2pos+lineNumber[ni] < lengthOfLine && lastMine[n2pos] == 0) n2pos++;
+			if(n1pos == n2pos)//数字确定
+			{
+				if(n1pos > 0 && lineOpen[n1pos-1] != 1)//头部翻开
+				{
+					lineSolution[n1pos-1] = 1;
+				}
+				for(i=n1pos; i < n1pos+lineNumber[ni]; i++)//中部标记
+				{
+					if(lineOpen[i] != 2) lineSolution[i] = 2;
+				}
+				if(n1pos+lineNumber[ni] < lengthOfLine && lineOpen[n1pos+lineNumber[ni]] != 1)//尾部翻开
+				{
+					lineSolution[n1pos+lineNumber[ni]] = 1;
+				}
+			}
+			else if(n2pos - n1pos < lineNumber[ni])//数字中部确定
+			{
+				for(i=n2pos; i < n1pos+lineNumber[ni]; i++)//中部标记
+				{
+					if(lineOpen[i] != 2) lineSolution[i] = 2;
+				}
+			}
+			if(ni == 0 && n1pos > 0)//线首翻开
+			{
+				for(i=0; i<n1pos; i++)
+				{
+					if(lineOpen[i] != 1) lineSolution[i] = 1;
+				}
+			}
+			if(ni == countOfLineNumber-1 && n2pos+lineNumber[ni] < lengthOfLine)//线尾翻开
+			{
+				for(i=n2pos+lineNumber[ni]; i<lengthOfLine; i++)
+				{
+					if(lineOpen[i] != 1) lineSolution[i] = 1;
+				}
+			}
+			n1pos += lineNumber[ni];
+			n2pos += lineNumber[ni];
+		}
+	}
+	//完成分析//中级测试种子7
+	int sum1 = 0, sum2 = 0;
+	for(i=0; i<lengthOfLine; i++)
+	{
+		if(lineOpen[i] == 2) sum1++;
+	}
+	for(i=0; i<countOfLineNumber; i++)
+	{
+		sum2 += lineNumber[i];
+	}
+	if(sum1 == sum2)
+	{
+		for(i=0; i<lengthOfLine; i++)
+		{
+			if(lineOpen[i] == 0 && lineSolution[i] == 0)
+			{
+				lineSolution[i] = 1;
+			}
+		}
+	}
+	if(debug)
+	{
+		printf("首解：");
+		for(i=0; i<lengthOfLine; i++)
+		{
+			printf("%d ", firstMine[i]);
+		}
+		printf("\n");
+		printf("末解：");
+		for(i=0; i<lengthOfLine; i++)
+		{
+			printf("%d ", lastMine[i]);
+		}
+		printf("\n");
+		printf("  解：");
+		for(i=0; i<lengthOfLine; i++)
+		{
+			if(lineOpen[i] == 2 || lineSolution[i] == 2) 
+			{
+				ColorNumber(lineSolution[i], 0x06);
+				putchar(' ');
+			}
+			else printf("%d ", lineSolution[i]);
+		}
+		printf("\n");
+	}
+	//标准线解写出
+	RecoverLine(li.r, li.c, 1);
+	free(firstMine);
+	free(lastMine);
+	free(hasfirstSolution);
+	//返回是否存在解
+	for(i=0; i<lengthOfLine; i++)
+	{
+		if(lineSolution[i] != 0) return 1;
+	}
+	return 0;
+}
+
+int SolveStep()
+{
+	int isSolving = 0;
+	struct LinesIterator li;
+	for(li = LinesIteratorBegin(); !IsLinesIteratorEnd(li); LinesIteratorNext(&li))//通过标准线组迭代器遍历
+	{
+		isSolving += SolveLine(li);
+	}
+	return isSolving != 0;
+}
+
+int Solve()
+{
+	int r, c;
+	int isSolving = 1;
+	for(r=0; r<heightOfBoard; r++)
+	{
+		for(c=0; c<widthOfBoard; c++)
+		{
+			solution[r][c] = 0;
+		}
+	}
+	while(isSolving)
+	{
+		isSolving = SolveStep();
+	}
+	for(r=0; r<heightOfBoard; r++)
+	{
+		for(c=0; c<widthOfBoard; c++)
+		{
+			if(isOpen[r][c] == 0 && solution[r][c] == 0) return 0;
+		}
+	}
+	return 1;
+}
+
 int main()
 {
 	int choiceMode;
@@ -545,6 +886,7 @@ int main()
 			clrscr();
 			//isSigning = 0;
 			seed = time(0);
+			//seed = 7;
 			remainder = numberOfMine;
 			SummonBoard(seed);
 			SetConsoleMouseMode(1);
@@ -669,6 +1011,11 @@ int main()
 								break;
 							}
 						}
+						else if(rcd.Event.KeyEvent.wVirtualKeyCode == '\t')
+						{
+							SolveStep();
+							break;
+						}
 					}
 					if(isReadyRefreshMouseOperatedPos == 1)
 					{
@@ -678,7 +1025,8 @@ int main()
 					showCursor(0);
 					Sleep(RefreshCycle);
 				}
-				if(isMine[r][c] == 1 && isOpen[r][c] == 1)
+				if(r>=0 && r<heightOfBoard && c>=0 && c<widthOfBoard
+					&& isMine[r][c] == 1 && isOpen[r][c] == 1)
 				{
 					break;//翻开雷失败
 				}
@@ -764,7 +1112,7 @@ int main()
 		}
 		else if(choiceMode == 4)
 		{
-			printf("[地图生成校验：0关闭/1必存在顶满边/2必存在空线]>");
+			printf("[地图生成校验：0关闭/1必存在顶满边/2必存在空线/3地图可解]>");
 			scanf("%d", &summonCheckMode);
 		}
 		else// if(choiceMode == 3)
@@ -794,6 +1142,8 @@ Nonogram 0.3
 Nonogram 0.4
 ——新增 鼠标点击数字时切换数字暗淡/明亮
 ——新增 地图辅助线
+——新增 可按Tab自动推进一定数织进度
+——新增 地图生成可校验地图可解
 ——优化 统一计算数字长度
 ——优化 颜色仅在地图生成时计算
 ——优化 地图大小上限从20*20提升到24*24
